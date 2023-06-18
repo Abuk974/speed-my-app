d543c2cc94caf465e0a28531ffe20e5314288eee
Abuk974 <120749240+Abuk974@users.noreply.github.com>
2023-06-18 08:56:42 +0300

    Added git repository Url

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+
       <h1>React Weather App</h1>
+      
+      <footer>
+      This project was coded by Abuk Sedonia and it is {""}
+      <a href="https://github.com/Abuk974/speed-my-app?" target="blank"> 
+      open-sourced on GitHub
+      </a>
+      </footer>
     </div>
   );
 }
